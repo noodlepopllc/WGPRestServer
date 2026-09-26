f3184340cf055b101f2b6e0b812885ca30adbd45
Todd Thomas <noodlepopllc@gmail.com>
2026-09-26 01:42:35 -0400

    Forcing profile 4+

--- wangp_server/worker.py
+++ wangp_server/worker.py
@@ -117,6 +117,7 @@ def main_worker():
                 "python",
                 "wgp.py",
                 "--process", "tmp.json",
+                "--profile", "4+",
                 "--output-dir", output_dir,
                 "--verbose", "0"
             ],
